4782b54a1a924ed84f6164a060b2ae6f8c96c6c5
2070super <74180810+2070super@users.noreply.github.com>
2022-02-25 00:29:46 +0800

    Add files via upload
    面部识别存储版

--- face.py
+++ face.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 cap=cv2.VideoCapture(0)
 kernel=np.ones((5,5),np.uint8)
+num=0
 while(True):
     hx,frame=cap.read()
     if hx is False:
@@ -10,9 +11,15 @@ while(True):
     img=frame.copy()
     frame=cv2.cvtColor(frame ,cv2.COLOR_BGR2GRAY)
     facedata=cv2.CascadeClassifier('C://Users//admin//PycharmProjects//parting//venv//face.xml')
-    faceRect=facedata.detectMultiScale(frame,1.1,7)
+    faceRect=facedata.detectMultiScale(frame,scaleFactor=1.1,minNeighbors=7)
     for (x,y,w,h) in faceRect:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0))
+        name='%s/%d.jpg'%('C://Users//admin//Desktop//face',num)
+        image=frame[y-10:y+h+10,x-10:x+w+10]
+        cv2.imwrite(name,image)
+        num+=1
+        if num>1000:
+            break
     cv2.namedWindow("videos",cv2.WINDOW_AUTOSIZE)
     cv2.imshow('videos',img)
     if cv2.waitKey(1) & 0xFF==ord('q'):
